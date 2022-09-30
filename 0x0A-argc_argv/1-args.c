19750c22a565f2bf6875ae5b0197ee253376973e
attahchisom6 <attahchisom6@gmail.com>
2022-09-30 22:23:30 +0100

    printing no of passed argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,14 +4,14 @@
 /**
  * main - prints the number of arguments passed to it
  * argc:Number of arguments
- * argv:array of arguments
+ * @argv:array of arguments
  * Return:0 on succsess
  */
 
 int main(int argc, char *argv[])
 {
 	(void) argv; /*ignore argv*/
-	printf("%i\n", argc - 1);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
