94504cf45cc4c2fd976d1e450910c85c2910dbb1
attahchisom6 <attahchisom6@gmail.com>
2022-09-30 21:25:07 +0100

    printing programme name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,15 +3,15 @@
 
 /**
  * main - function to print name of programme
- * @argc:No. of arguments
+ * @argc:number of arguments
  * @argv:array of argument
- * 
- * Return:0 if successful execution
+ * Return:0 on successful execution
  */
 
 int main(int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
