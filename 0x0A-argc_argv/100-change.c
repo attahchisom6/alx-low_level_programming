c1ebbcdab4abc23a5e4875119a6d7288c95841bf
attahchisom6 <attahchisom6@gmail.com>
2022-10-01 02:14:14 +0100

    getting a change or remainder from a given set of array

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to get remainder from a given
+ * list of numbers
+ * @argc:size of argv
+ * @argv:array or list of numbers
+ *
+ * Return:0
+ */
+
+int main(int argc, char *argv[])
+{
+	int total, change, p, k;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	total = atoi(argv[1]);
+	change = 0;
+
+	if (total < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (k = 0; k < 5 && total >= 0; k++)
+	{
+		while (total >= coins[k])
+		{
+			change++;
+			total -= coins[k];
+		}
+		
+	}
+	printf("%d\n", change);
+
+	return (0);
+}
