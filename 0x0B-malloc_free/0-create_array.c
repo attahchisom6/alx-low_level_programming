3347225214b6b60a2959d0fe90b5c187e452f36b
attahchisom6 <attahchisom6@gmail.com>
2022-10-03 19:40:52 +0100

    dynamic array: printing a character from an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int k;
 	char *arr;
 
-	arr =  malloc(size*sizeof(char));
+	arr =  malloc(size * sizeof(char));
 	if (size == 0)
 	{
 		return (NULL);
