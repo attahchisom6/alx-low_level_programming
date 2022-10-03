64bca1b458af9f560063fae31c098196145b5621
attahchisom6 <attahchisom6@gmail.com>
2022-10-03 23:42:26 +0100

    dynamic string:duplicating string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - This function will duplicate the string parameter
+ * passed to it
+ * @str:string to be duplicated
+ *
+ * Return:pointer to new created memory for duplicate string
+ */
+
+char *_strdup(char *str)
+{
+	int k, m, p;
+	char *strc; /*new to be copied from str*/
+
+	p = 0;
+	while (str[p] != '\0')
+	{
+		p++;
+	}
+
+	k = p + 1; /*total length of str including null terminator*/
+	strc = malloc(k * sizeof(char));
+	for (m = 0; m < k; m++)
+	{
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+		strc[m] = str[m];
+	}
+	free(strc);
+	return (strc);
+}
