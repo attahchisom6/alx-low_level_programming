5f7d79bba101cd3d8fc8e1cbd1805046fb4c70ef
attahchisom6 <attahchisom6@gmail.com>
2022-10-10 18:00:48 +0100

    my dog data

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure containing my data tpes
+ * @name:name of dog
+ * @age:Age of dog
+ * @owner:name of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
