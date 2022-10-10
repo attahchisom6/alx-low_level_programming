c4713057f2e5cb7951e3e5a6b86cd72ee6414ecc
attahchisom6 <attahchisom6@gmail.com>
2022-10-10 21:06:47 +0100

    printing all the each data of the dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - this function print the names of variables
+ * passed to it
+ * @struct dog - A structure containing the information of the dog
+ * *@d:pointer to structure
+ */
+
+void print_dog(struct dog *d)
+{
+
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
