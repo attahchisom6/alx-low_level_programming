d7d91ab8732fa4a88a0bff6bb84381963f6ba842
attahchisom6 <attahchisom6@gmail.com>
2022-10-12 19:17:07 +0100

    function pointers: executing each funtion array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - this funcrion exexutes a funcrion passed
+ * as a parameter to each element of its array
+ * @arrray:of element
+ * @size:size of the array
+ * @action:pointer to the function paased
+ *
+ * Return:void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int p;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (p = 0; p < size; p++)
+		action(array[p]);
+}
