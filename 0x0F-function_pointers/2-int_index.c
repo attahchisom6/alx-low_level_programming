da80036cd596bfa9fc57ebf53c0a3ac42311628c
attahchisom6 <attahchisom6@gmail.com>
2022-10-13 00:39:20 +0100

    function:searching by comparisont

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int k;
 
-	if (size <= 0 || cmp == NULL)
+	if (array == NULL || cmp == NULL)
+		return;
+
+	if (size <= 0)
 		return (-1);
 
 	for (k = 0; k < size; k++)
@@ -23,5 +26,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[k]) > 0)
 			return (k);
 	}
-	return (1);
+	return (-1);
 }
