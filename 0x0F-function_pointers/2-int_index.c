1927aa6649a8e646903c47089a49638345fe2523
attahchisom6 <attahchisom6@gmail.com>
2022-10-12 23:57:56 +0100

    function:searching by comparison

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,28 +1,27 @@
-
-#include <stdio.h>
 #include "function_pointers.h"
+#include <stdio.h>
+
 /**
- * int_index - earches for an integer
- * @array: array to search in
- * @size: size of the array
- * @cmp: pointer to the comparing function
+ * int_index - funtion to search for ab integer
+ * @array:array of elements containing number to be searched
+ * @size:number of elements of the  array
+ * @cmp:pointer to the function to be used to compare values
  *
- * Return: index of the first element for which
- * the cmp function does not return 0, or -1 if no match is found
- * or size is negative
+ * Return:index of first element for which cmp does not return 0
+ * -1 if no matching element is found or size <= 0
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int k;
 
-	if (array && cmp)
+	if (size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (k = 0; k < size; k++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
+		if (cmp(array[k]) > 0)
+			return (k);
 	}
-
-	return (-1);
+	return (1);
 }
