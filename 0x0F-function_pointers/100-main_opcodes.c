bf2fe1fd77f4342bce33cde2adad04ac974932e0
attahchisom6 <attahchisom6@gmail.com>
2022-10-13 03:26:33 +0100

    generating my machine upcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,42 +1,45 @@
+#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - prints its own opcodes
+ * main - prints the opcode of the user
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ * Return:0
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-	int bytes, i;
-	char *arr;
+	int k = 0, nbytes; /*nbytes = number of bytes*/
+	char *buffer;
+	
+	k = 0, buffer = (char *)main;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	bytes = atoi(argv[1]);
-
-	if (bytes < 0)
+	
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
+	while (k < nbytes)
 	{
-		if (i == bytes - 1)
+		while (k < (nbytes - 1))
 		{
-			printf("%02hhx\n", arr[i]);
-			break;
+			printf("%02x ", buffer[k]);
+			k++;
 		}
-		printf("%02hhx ", arr[i]);
+		printf("%02x\n", buffer[k]);
+		k++;
+		break;
 	}
 	return (0);
 }
