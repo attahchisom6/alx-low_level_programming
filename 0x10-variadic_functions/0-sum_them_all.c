5a7769a250b88241df94a05b161cfed86a4cadac
attahchisom6 <attahchisom6@gmail.com>
2022-10-13 14:55:53 +0100

    variadic:functions: adding all the argument

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - function to sum all the argument passed to it
+ * @n:first argument
+ * @...:all other argument
+ *
+ * Return:void
+ */
+
+int sum_them_all(const unsigned int n,...)
+{
+	unsigned int k, sum = 0;
+	va_list(ap);
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+	k = 0;
+	while (k < n)
+	{
+		sum = sum + va_arg (ap, int);
+		k++;
+	}
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,4 @@
+# **VARIADIC FUNCTIONS**
+
+_variadic function are function that take unknown number of argument at declaration.
+in a nutshell, it looks similar to dynamic memory allocation, where we don't know the amount of space to allocate initially_
