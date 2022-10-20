41927ad3b9457ec17c3f5014f990918ee374b42b
attahchisom6 <attahchisom6@gmail.com>
2022-10-21 00:27:08 +0100

    single linked list: appending nodes at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,8 +16,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	size_t p = 0;
 
 	s = strdup(str);
-/*initialize last element of list_t to point to head*/
-	end = *head;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
@@ -31,14 +29,15 @@ list_t *add_node_end(list_t **head, const char *str)
 /*link should here*/
 	new->next = NULL;
 /*if the link (head) is empty, then head point  to new link*/
-	if (*head == NULL)
+	end = *head;
+	if (end  == NULL)
 		*head = new;
 
 	else
 	{
 		while (end->next != NULL)
-			end = end->next;
-			end->next = new;
+		end = end->next;
+		end->next = new;
 	}
 
 	return (*head);
