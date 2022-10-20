01fbced3af8e7c8572808fb8b5fdc4d1ab85e804
attahchisom6 <attahchisom6@gmail.com>
2022-10-20 21:24:52 +0100

    singly list links: Adding link before head

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,23 +11,20 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	size_t p;
-	list_t *new_node;
+	list_t *new;
+	size_t a = 0;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
-	p = 0;
-	while (str[p] != '\0')
-		p++;
+	while (str[a])
+		a++;
 
-		new_node->str = strdup(str);
-		new_node->len = p;
-	/*make next of new node point to head*/
-		new_node->next = *head;
-	/*move head to poibt to the new node*/
-		*head = new_node;
+	new->str = strdup(str);
+	new->len = a;
+	new->next = *head;
+	*head = new;
 
 	return (*head);
 }
