d102eb91edf1702aebb8d1c5ea659cc16d8fe5ef
attahchisom6 <attahchisom6@gmail.com>
2022-10-20 20:35:54 +0100

    singly list links: Adding link before head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_note - this function will add a nods to the beginning of
+ * a linked list
+ * @head:first link
+ * @str:string to be added to link we want to add before head
+ *
+ * Return:Address of new elemwnt or NULL if it failals
+ */
+
+list_t *add_node(list_t **head, char *str)
+{
+	char *s;
+	size_t p;
+
+	s = strdup(str);
+	list_t *new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	p = 0;
+	while (str[p] != '\0')
+		p++;
+		new_node->str = s;
+		new_node->len = p;
+	/*make next of new node point to head*/
+		new_node->next = *head;
+	/*move head to poibt to the new node*/
+		*head = new_node;
+
+	return (*head);
+}
