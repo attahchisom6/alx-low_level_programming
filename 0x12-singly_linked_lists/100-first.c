e766894354e27a9eba9d95f52a21eb8edd58e66e
attahchisom6 <attahchisom6@gmail.com>
2022-10-21 02:52:56 +0100

    header file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -12,18 +12,3 @@ void print_before(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
-
-
-/**
- * main - fuction to print a string aft
-er the first function
-print its own string
- *
- * Return:0
- */
-
-int main(void)
-{
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
-}
