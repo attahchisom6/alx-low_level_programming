c64233ab0f7150437f9b6985606881660f0e9710
attahchisom6 <attahchisom6@gmail.com>
2022-10-21 03:02:42 +0100

    header file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,5 +10,6 @@ void print_before(void) __attribute__((constructor));
 
 void print_before(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
 }
