ffcd734d473bc462711db345de8ca4fdd9e5918b
attahchisom6 <attahchisom6@gmail.com>
2022-10-24 11:40:09 +0100

    printing number of elements in a link

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,4 +8,14 @@
  * Return:number of elements in the link
  */
 
-size_t list_len
+size_t listint_len(const listint_t *h)
+{
+	size_t p;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		p++;
+	}
+	return (p);
+}
