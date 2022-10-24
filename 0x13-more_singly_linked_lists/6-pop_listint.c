98d7ccdb813f7e988bf4164b8890cb5fa8dcdab3
attahchisom6 <attahchisom6@gmail.com>
2022-10-24 19:48:40 +0100

    deleting node from the begining

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - This functio  will delete a node from the
+ * begining of a link
+ * @h:pointer to a pointer to the firstnode of the link
+ *
+ * Return:link without the deleted node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	size_t p;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	p = (*head)->n;
+	temp = *head;/*hold head temporarily*/
+	*head = (*head)->next;/*move to the next node*/
+	free(temp);/*delete temp*/
+
+	return (p);
+}
