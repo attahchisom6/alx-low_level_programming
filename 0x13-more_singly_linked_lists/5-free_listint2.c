ec2740f9a5e5179bb055e725b41217db2a303a57
attahchisom6 <attahchisom6@gmail.com>
2022-10-24 16:47:14 +0100

    free pointer to a pointer that points to head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function to free a link
+ * @head:pointer to a pointer to first node
+ * 
+ * Return:void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
