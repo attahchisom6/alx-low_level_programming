971ca4532309c0beaf478d3bfccc4c587da7a98e
attahchisom6 <attahchisom6@gmail.com>
2022-10-25 16:36:01 +0100

    inserting a node at index idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,8 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			k++;
 		}
 
-		new->next = temp->next;
-		temp->next = new;
+		new = &(*new)->next;
 	}
 
 	return (new);
