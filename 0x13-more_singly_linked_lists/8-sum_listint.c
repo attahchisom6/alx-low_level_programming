3a273f6c39cf66e6d05c0dbd8b0476524c0bc406
attahchisom6 <attahchisom6@gmail.com>
2022-10-25 01:49:32 +0100

    adding the data srored in a link

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function to sum all the data contained in a list
+ * @head:pointer to the first node
+ *
+ * Return:sum of all the data
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *node;
+	int p, sum = 0;
+
+	node = head;
+	while (node != NULL)
+	{
+		sum = sum + node->n;
+		node = node->next;
+	}
+	return (sum);
+}
