a60b8e17041bff45531ce24016fb0b310fdfcac2
attahchisom6 <attahchisom6@gmail.com>
2022-10-25 14:57:48 +0100

    inserting a node at index idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,15 +23,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head == NULL && idx != 0)
 		return (NULL);
 	else if (*head == NULL && idx == 0)
-	{
 		*head = new;
-		return (new);
-	}
 	else if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
-		return (new);
 	}
 	else
 	{
