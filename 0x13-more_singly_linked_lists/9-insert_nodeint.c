9fb322ef557ff27544dc20217512751ff92bce31
attahchisom6 <attahchisom6@gmail.com>
2022-10-25 15:21:18 +0100

    inserting a node at index idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		/*we will now exclude case k = 0*/
 		/*i.e we  will start at case k + 1*/
 	temp = *head;
-	while (k < idx - 1)
+	while (k < (idx - 1))
 	{
 		temp = temp->next;
 		k++;
