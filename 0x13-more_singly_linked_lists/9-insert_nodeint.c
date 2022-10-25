39d8e452d545dd19ecfe28582a119c0c60d80c0e
attahchisom6 <attahchisom6@gmail.com>
2022-10-25 14:15:57 +0100

    inserting a node at index idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,8 +28,7 @@ int node_len(listint_t *h)
  * Return:pointer to the inserted  node
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head,
-		unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *temp;
 	size_t k = 0;
