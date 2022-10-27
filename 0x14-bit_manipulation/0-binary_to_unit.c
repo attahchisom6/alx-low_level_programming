c91c63b4dd727195717b856d9ea709cdb0104853
attahchisom6 <attahchisom6@gmail.com>
2022-10-27 18:08:02 +0100

    converting binary to base 10

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - function to convert binary numbers to decimal
+ * @b:string of binary digits(containing only 0 or 1.
+ *
+ * Return:decimal equivalent of binary
+ */
+  unsigned int binary_to_uint(const char *b)
+{
+	unsigned int p = 1, deci = 0;
+	int k = 0, m = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[k] != '\0')
+		k++;
+
+	k--;
+	for (; k >= 0; k--)
+	{
+		if (b[k] == '0')
+		{
+			p *= 2;
+			continue;
+		}
+		else if (b[k] == '1')
+		{
+			deci += p;
+			p *= 2;
+			continue;
+		}
+		return (0);
+	}
+	return (deci);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,4 @@
+# **BINARY MANIPULATION**
+
+_binary manipulation invoves the use of binary operation on an integer or character on a bit level_
+_note that binary manipulation is not compatible with float or double_
