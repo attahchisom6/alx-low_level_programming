59bc5a9edf8f54a97b758f73a50369c932f29a41
attahchisom6 <attahchisom6@gmail.com>
2022-10-31 18:35:02 +0100

    reading and writing out a number of chars from buffer

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * read_textfile - function to read text file and print to posix stdout
+ * @filename:file to be raed from
+ * @letters:letters to be printed from filename
+ *
+ * Return:chars to stdout and their total number
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t fd, k, p; /*where fd is field descriptor*/
+	char *buffer;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buffer = malloc(letters * sizeof(char));
+	if (buffer == NULL)
+		return (0);
+	p = read(fd, buffer, letters);
+	k = write(STDOUT_FILENO, buffer, p);
+
+	close(fd);
+	free(buffer);
+	return (k);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+ssize_t read_textfile(char const *filename, size_t letters);
+
+#endif
