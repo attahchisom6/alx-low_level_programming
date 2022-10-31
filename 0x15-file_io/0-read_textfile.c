23f0b15a32a36f5b179d2e52ba955204256e77e7
attahchisom6 <attahchisom6@gmail.com>
2022-10-31 19:12:27 +0100

    reading and writing text from a textfile to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,19 +30,3 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buffer);
 	return (p);
 }
-
-int main(int ac, char **av)
-{
-    ssize_t n;
-
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %i)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %i)\n", n);
-    return (0);
-}
