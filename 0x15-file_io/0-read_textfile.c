ffa709faf764b74703e3ce539384d454ca8407c0
attahchisom6 <attahchisom6@gmail.com>
2022-10-31 19:09:10 +0100

    reading and writing text from a textfile to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,8 +23,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	p = read(fd, buffer, letters);
 	k = write(STDOUT_FILENO, buffer, p);
+	if (k < 0)
+		return (0);
 
 	close(fd);
 	free(buffer);
 	return (p);
+}
+
+int main(int ac, char **av)
+{
+    ssize_t n;
+
+    if (ac != 2)
+    {
+        dprintf(2, "Usage: %s filename\n", av[0]);
+        exit(1);
+    }
+    n = read_textfile(av[1], 114);
+    printf("\n(printed chars: %i)\n", n);
+    n = read_textfile(av[1], 1024);
+    printf("\n(printed chars: %i)\n", n);
+    return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(char const *filename, size_t letters);
 
