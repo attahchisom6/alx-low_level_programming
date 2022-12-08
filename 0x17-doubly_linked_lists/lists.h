7de84798c444ce292ce0dd5e64bd28812cfce21c
attahchisom6 <attahchisom6@gmail.com>
2022-12-08 18:05:25 +0100

    printing elements of a doubly linked list

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct dlistint_s - a double linked list
+ * @prev:pointer to the previous node
+ * @mext:pointer to the next node
+ *
+ * Description:a linked links with the attribute of double
+ * tranversal
+ */
+
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
