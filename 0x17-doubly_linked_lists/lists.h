2194f1dc962ae8f0c24fdac9bb789984fd30b552
attahchisom6 <attahchisom6@gmail.com>
2022-12-09 03:58:11 +0100

    my header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
- * struct dlistint_s - a double linked list
- * @prev:pointer to the previous node
- * @mext:pointer to the next node
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
  *
- * Description:a linked links with the attribute of double
- * tranversal
+ * Description: doubly linked list node structure
+ * for Holberton project
  */
 
 typedef struct dlistint_s
@@ -21,6 +21,7 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -29,5 +30,6 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif
+#endif /* LISTS_H */
