63a6f3a9387976a8454d270fe1c9bdc275dd7e4b
attahchisom6 <attahchisom6@gmail.com>
2022-12-09 03:34:22 +0100

    inserting index in a DLL

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert a node at the given index
+ * @idx:index
+ * @n:integer data to insert at the new node
+ *
+ * Return:address of new node, on failure return NULL
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new, *end, *begin;
+	size_t k = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		free(h);
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *h;
+		new->prev = NULL;
+		(*h)->prev = new;
+		*h = new;
+
+		return (new);
+	}
+
+	end = *h;
+	while (end != NULL && k < idx)
+	{
+		if (k == idx - 1)
+		{
+			new->next = end->next;
+			end->next = new;
+			new->prev = end;
+			if (new != NULL)
+				new->next->prev = new;
+				
+			return(new);
+		}
+		else
+			end = end->next;
+		k++;
+
+	return (NULL);
+}
