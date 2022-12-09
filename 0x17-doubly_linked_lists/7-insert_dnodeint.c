30b11b91030971d7d77e4b1e6a1bc77339ac9aaa
attahchisom6 <attahchisom6@gmail.com>
2022-12-09 18:02:40 +0100

    inserting a node at a given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new = add_dnodeint_end(h, n);
 			return (new);
 		}
-		else if ((idx - 1) = k)
+		else if ((idx - 1) == k)
 		{
 			new->next = end->next;
 			new->prev = end;
