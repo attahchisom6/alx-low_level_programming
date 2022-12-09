1bb97ed064a837ff2f0798e29b5fb7e5f75edfbc
attahchisom6 <attahchisom6@gmail.com>
2022-12-09 03:59:49 +0100

    my header file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *end, *begin;
-	size_t k = 0;
+	unsigned k = 0;
 	
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
