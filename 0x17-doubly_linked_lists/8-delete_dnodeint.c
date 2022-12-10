57a89659e87c3cd742f99a8a3b32244ffb7633bc
attahchisom6 <attahchisom6@gmail.com>
2022-12-10 09:21:17 +0100

    deleting node at a given index in DLL

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,54 +1,106 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Delete node at nth index
- *
- * @head: Head of node
- *
- * @index: index
- *
- * Return: 1 succeed, -1 if fail
+ * delete_dnodeint_at_index - function to delete a node
+ * at position index
+ * @head:a double pointer to the first node
+ * @index: position of node to be deleted
+ * Return:1 on success, -1 on failure
  */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *node;
-	unsigned int count;
+	dlistint_t *node_nxt, *temp, *temp_1;
+	unsigned int k = 0;
 
 	if (*head == NULL)
 		return (-1);
 
-	node = *head;
 	if (index == 0)
 	{
-		*head = node->next;
-		if (node->next != NULL)
-		{
-			node->next->prev = NULL;
-		}
-		free(node);
-		return (1);
-	}
-	for (count = 0; node != NULL && count < index - 1 ; count++)
-	{
-		node = node->next;
-	}
-	if (node == NULL || node->next == NULL)
-	{
-		return (-1);
+		temp_1 = *head;
+		*head = temp_1->next;
+		if (temp_1->next != NULL)
+			temp_1->next->prev = NULL;
+		free(temp_1);
 	}
 
-	if (node->next->next != NULL)
-	{
-		node->next = node->next->next;
-		free(node->next->prev);
-		node->next->prev = node;
-		return (1);
-	}
 	else
 	{
-		free(node->next);
-		node->next = NULL;
+		temp = *head;
+		while (k < index - 1)
+		{
+			if (temp == NULL || temp->next == NULL)
+				return (-1);
+			temp = temp->next;
+			k++;
+		}
+
+		node_nxt = temp->next;
+		temp->next = node_nxt->next;
+		node_nxt->next->prev = temp;
+		free(node_nxt);
+
 		return (1);
 	}
 	return (-1);
+}
+
+int main(void)
+{
+    dlistint_t *head;
+
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 5);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    printf("-----------------\n");
+    delete_dnodeint_at_index(&head, 0);
+    print_dlistint(head);
+    return (0);
 }
