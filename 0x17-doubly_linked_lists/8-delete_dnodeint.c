7884ed41b61d0ae05cb0f62c2396ad9e79c9ea6e
attahchisom6 <attahchisom6@gmail.com>
2022-12-10 17:00:03 +0100

    deleting a node at a given index in DLL

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,9 +15,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (*head == NULL || head == NULL)
 		return (-1);
+	temp_1 = *head;
 	if (index == 0)
 	{
-		temp_1 = *head;
 		*head = temp_1->next;
 		if (temp_1->next != NULL)
 			temp_1->next->prev = NULL;
