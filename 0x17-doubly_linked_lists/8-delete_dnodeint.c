323170050267ba52cda8a3f9571e357b0c164086
attahchisom6 <attahchisom6@gmail.com>
2022-12-10 16:11:54 +0100

    deleting a node at a given index in DLL

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,53 +1,51 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Delete node at nth index
- *
- * @head: Head of node
- *
- * @index: index
- *
- * Return: 1 succeed, -1 if fail
+ * delete_dnodeint_at_index - function to delete a node
+ * at position index
+ * @head:a double pointer to the first node
+ * @index: position of node to be deleted
+ * Return:1 on success, -1 on failure
  */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *node;
-	unsigned int count;
+	dlistint_t *temp, *temp_1, *del;
+	unsigned int k = 0;
 
 	if (*head == NULL)
 		return (-1);
-
-	node = *head;
 	if (index == 0)
 	{
-		*head = node->next;
-		if (node->next != NULL)
-		{
-			node->next->prev = NULL;
-		}
-		free(node);
+		temp_1 = *head;
+		*head = temp_1->next;
+		if (temp_1->next != NULL)
+			temp_1->next->prev = NULL;
+		free(temp_1);
 		return (1);
 	}
-	for (count = 0; node != NULL && count < index - 1 ; count++)
+
+	temp = *head;
+	while (k < index - 1)
 	{
-		node = node->next;
-	}
-	if (node == NULL || node->next == NULL)
-	{
-		return (-1);
+		if (temp == NULL)
+			return (-1);
+		temp = temp->next;
+		k++;
 	}
 
-	if (node->next->next != NULL)
+	if (temp->next != NULL)
 	{
-		node->next = node->next->next;
-		free(node->next->prev);
-		node->next->prev = node;
+		del = temp->next;
+		temp->next = del->next;
+		del->next->prev = temp;
+		free(del);
 		return (1);
 	}
 	else
 	{
-		free(node->next);
-		node->next = NULL;
+		free(temp->next);
+		temp->next = NULL;
 		return (1);
 	}
 	return (-1);
