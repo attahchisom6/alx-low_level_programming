76e57e0350cc282118f7d7b4a3ec4a04a4b221a4
attahchisom6 <attahchisom6@gmail.com>
2023-01-05 20:51:12 +0100

    debbugged it

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -7,31 +7,31 @@
 
 /**
  * struct hash_node_s - Node of a hash table
- * @key: A fixed string
- * @value:value to be assigned to key
- * @next: a pointer to the next node
- * Description: a singly linked list detailing the items contained in a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
  */
-
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+     char *key;
+     char *value;
+     struct hash_node_s *next;
 } hash_node_t;
 
 /**
- * struct hash_table_s - a hash table structure
- * @size: size of the hash table
- * @array: array of pointers to the first node
- * Description: a hash data holding size of the hash table and a double pointer to the first elements
- * because we want our handling to use a collision collection handling
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
  */
-
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+     unsigned long int size;
+     hash_node_t **array;
 } hash_table_t;
 
 /*str_func.c*/
