8edaa0cacdd346663c7256f0a5e2fe9c47271e41
attahchisom6 <attahchisom6@gmail.com>
2023-01-06 18:30:38 +0100

    printing elements of a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the elements of a hash table
+ * @ht: a pointer to the hash table
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int k = 0;
+	hash_node_t *head;
+	char *delim = "";
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	while (k < ht->size)
+	{
+		head = ht->array[k];
+		while (head != NULL)
+		{
+			printf("%s'%s': '%s'", delim, head->key, head->value);
+			delim = ", ";
+
+			head = head->next;
+		}
+		k++;
+	}
+	printf("}\n");
+}
