2cc12200e1f20d0c45e03e4f7787fbe2be411d44
attahchisom6 <attahchisom6@gmail.com>
2023-01-06 08:53:19 +0100

    getting the index a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - function to get the index of a key
+ * @key: fixed value if a string
+ * @size: size of the hash taable
+ *
+ * Return: index of the key
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int k;
