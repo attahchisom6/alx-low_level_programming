d204b71e24dc567e763db016390c57dd292b2bac
attahchisom6 <attahchisom6@gmail.com>
2023-01-06 15:58:57 +0100

    getting the value of a hash table with a given key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -24,7 +24,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(head->key, key) == 0)
 			return (head->value);
-		head = head->next
+		head = head->next;
 	}
 	return (NULL);
 }
