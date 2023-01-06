8ce25b675febf1114ed3c91920febd049ee72b37
attahchisom6 <attahchisom6@gmail.com>
2023-01-06 15:06:10 +0100

    getting the value of a hash table with a given key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function to get a value of a given key
+ * @key: the key we seek
+ * @value: value of the key
+ *
+ * Return: pointer to the value if found else NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int k, size, index;
+
+	if (ht == NULL)
+		return (NULL);
+
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+
+	k = index;
+	while (ht->array[k] != NULL)
+	{
+		if (strcmp(ht->array[k]->key, key) == 0)
+			return (ht->array[k]->value);
+		k++;
+	}
+	return (NULL);
+}
