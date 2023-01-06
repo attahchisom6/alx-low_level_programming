2968d7cdc0ca0fb2570c713b203ecff93c359792
attahchisom6 <attahchisom6@gmail.com>
2023-01-06 19:58:17 +0100

    deleting elements of a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - This function deletes an element from a hash table
+ * @ht: a pointer to the hash table
+ *
+ * Void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int k = 0;
+	hash_node_t *head, *temp;
+
+	if (head == NULL)
+		return;
+
+	while (k < ht->size)
+	{
+		head = ht->array[k];
+		while (head != NULL)
+		{
+			temp = head;
+			head = head->next;
+			free(temp);
+		}
+		k++;
+	}
+}
