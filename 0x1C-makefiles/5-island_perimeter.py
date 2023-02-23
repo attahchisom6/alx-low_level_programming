0b0758103d733b74ce0ffa453627c10d0b5ebc95
attahchisom6 <attahchisom6@gmail.com>
2023-02-23 19:02:33 +0100

    got the perimeter of a grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""this module defines to get the perimeter of an islor"""
+
+
+def island_perimeter(grid):
+    """function to get the perimwter of an islor described in a grid"""
+
+    perimeter = 0
+    """
+    h_link reperesent horizontal link or connection i.e rows
+    v_link: represent vertical link or connection i.e column
+    """
+    h_link = len(grid)
+    if grid != []:
+        v_link = len(grid[0])
+
+    for p in range(h_link):
+        for k in range(v_link):
+            if grid[p][k] == 1:
+                if p == 0 or grid[-1][k] == 0:
+                    perimeter += 1
+                if p == 0 or grid[1][k] == 0:
+                    perimeter += 1
+
+                if k == 0 or grid[p][-1] == 0:
+                    perimeter += 1
+                if k == 0 or grid[p][1] == 0:
+                    perimeter += 1
+
+    return perimeter
