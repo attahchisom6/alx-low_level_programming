ddec70c232d5409b74ac94564dc8a5a58bc19aa9
attahchisom6 <attahchisom6@gmail.com>
2023-04-11 21:57:54 +0100

    good

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/**
+ * a function that searches for a value in a an array using a linear search
+ * algorithm
+ * @array: an array of integers
+ * @size:number of elements in the array
+ * value: whose index we seek in the array
+ *
+ * Return: index else -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t k = 0;
+
+	if (!array)
+		return (-1);
+
+	while (k < size)
+	{
+		if (array[k] == value)
+		{
+			return (k);
+			break;
+		}
+		k++;
+	}
+	return (-1);
+}
